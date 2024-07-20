dc5b946c40956bc5371f306765c073a468ef2018
Parvesh Qaiser <parvesh@entrolabs.com>
2024-07-20 17:29:34 +0530

    firebase key protetcted

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,12 +4,12 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBI2TXq5xf30WLh-q13h2UtZaM2Ew3kHo8",
+    apiKey: process.env.FIREBASE_KEY,
     authDomain: "foodpanda-9c4f8.firebaseapp.com",
     projectId: "foodpanda-9c4f8",
     storageBucket: "foodpanda-9c4f8.appspot.com",
     messagingSenderId: "501463467137",
-    appId: "1:501463467137:web:dbf3d3807eb8647815b980",
+    appId: process.env.FIREBASE_ID,
     measurementId: "G-74CVQMJZSD"
 };
 
